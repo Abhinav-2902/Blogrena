ebcb7b005a052b5980524db9bd5f32568f396d6d
Abhinav-2902 <abhikumar2302@gmail.com>
2025-08-19 09:59:48 +0530

    initial

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./header/Header/LogoutBtn"
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm
+}
